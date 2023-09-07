7b0ba3d789ffa9c327f3810dc635506bb6a9d00f
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-09-07 18:25:31 +0300

    stle my navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,13 @@ function NavBar() {
     <div className="logo"> 
     <p> Koro</p>
     <NavLink to = "/home"><h5> Home </h5></NavLink>
-    <NavLink to = "/about"><h5> Home </h5></NavLink>
-    <NavLink to = "/services"><h5> Home </h5></NavLink>
-    <NavLink to = "/home"><h5> Home </h5></NavLink>
-    <NavLink to = "/home"><h5> Home </h5></NavLink>
-    <NavLink to = "/home"><h5> Home </h5></NavLink>
-    <NavLink to = "/home"><h5> Home </h5></NavLink>
-    <NavLink to = "/home"><h5> Home </h5></NavLink>
+    <NavLink to = "/about"><h5> About </h5></NavLink>
+    <NavLink to = "/services"><h5> Services </h5></NavLink>
+    <NavLink to = "/downloads"><h5> Downloads </h5></NavLink>
+    <NavLink to = "/events"><h5> Events </h5></NavLink>
+    <NavLink to = "/projects"><h5> Projects </h5></NavLink>
+    <NavLink to = "/offerings"><h5> Offerings </h5></NavLink>
+    <NavLink to = "/contact"><h5> Contact </h5></NavLink>
 </div>
     </div>
   
