d542af82bf4f8338fcaa7a2aa4c9851ee7f429ce
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-01 15:50:53 +0300

    add media queries

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import closeIcon from '../assets/nav/closeIcon.png';
+import menuIcon from '../assets/nav/menuIcon.png';
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="navbar"> 
       <a href="/" className="navbar-title">St. James Koromosho</a>
-    <div className="nav-links"> 
-    <ul>
-      <li>
-        <a href="/">HOME</a>
-      </li>
-
-      <li>
-        <a href="/">ABOUT</a>
-      </li>
-
-      <li>
-        <a href="/">MINISTRIES</a>
-      </li>
-
-      <li>
-        <a href="/">SERMONS</a>
-      </li>
-
-      <li>
-        <a href="/">EVENTS</a>
-      </li>
-
-      <li>
-        <a href="/">CONTACT</a>
-      </li>
-    </ul>
+      <div className="nav-links"> 
+        <img src={menuOpen ? closeIcon : menuIcon} alt="menu" onClick={() => setMenuOpen(!menuOpen)} />
+        {menuOpen && (
+          <ul className={`menu-list ${menuOpen ? 'open' : 'closed'}`} onClick={() => setMenuOpen(!menuOpen)}>
+            <li>
+              <a href="/">HOME</a>
+            </li>
+            <li>
+              <a href="#about">ABOUT</a>
+            </li>
+            <li>
+              <a href="#ministries">MINISTRIES</a>
+            </li>
+            <li>
+              <a href="#sermons">SERMONS</a>
+            </li>
+            <li>
+              <a href="#events">EVENTS</a>
+            </li>
+            <li>
+              <a href="#contact">CONTACT</a>
+            </li>
+          </ul>
+        )}
+      </div>
     </div>
-
-    </div>
-  
   );
 }
 
