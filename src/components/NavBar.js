5b3956ab9564a7c6720336490bdb17be4ce3089e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-01 10:41:33 +0300

    change font style to Outfit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <div className="navbar"> 
-      <a href="/">St. James Koromosho</a>
+      <a href="/" className="navbar-title">St. James Koromosho</a>
     <div className="nav-links"> 
     <ul>
       <li>
